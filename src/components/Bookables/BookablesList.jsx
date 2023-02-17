226b6d073303713defcb987ecdb26eef64b6a072
Augusto Acual <augustoacual@gmail.com>
2023-02-16 19:13:32 -0600

    Triggering a UI update when changing the selected room - useState

--- src/components/Bookables/BookablesList.jsx
+++ src/components/Bookables/BookablesList.jsx
@@ -1,20 +1,16 @@
 import { bookables } from '../../static.json'
+import { useState } from 'react'
 
 export default function BookablesList() {
   const group = 'Rooms'
   const bookablesInGroup = bookables.filter((b) => b.group === group)
-  let bookableIndex = 1
-
-  function changeBookable(selectedIndex) {
-    bookableIndex = selectedIndex
-    console.log(selectedIndex)
-  }
+  const [bookableIndex, setBookableIndex] = useState(1)
 
   return (
     <ul className="bookables items-list-nav">
       {bookablesInGroup.map((b, i) => (
         <li key={b.id} className={i === bookableIndex ? 'selected' : null}>
-          <button className="btn" onClick={() => changeBookable(i)}>
+          <button className="btn" onClick={() => setBookableIndex(i)}>
             {b.title}
           </button>
         </li>
