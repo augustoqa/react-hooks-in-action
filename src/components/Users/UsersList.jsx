7ce71e574cc775b83fe5552e1d1c99f9001e6863
Augusto Acual <augustoacual@gmail.com>
2023-02-25 17:40:12 -0600

    Show Users Details

--- src/components/Users/UsersList.jsx
+++ src/components/Users/UsersList.jsx
@@ -1,18 +1,36 @@
 import { users } from '../../static.json'
-import { useState } from 'react'
+import { useState, Fragment } from 'react'
 
 export default function UsersList() {
   const [userIndex, setUserIndex] = useState(0)
 
+  const user = users[userIndex]
+  console.log(user)
+
   return (
-    <ul className="users items-list-nav">
-      {users.map((user, i) => (
-        <li key={user.id} className={i === userIndex ? 'selected' : null}>
-          <button className="btn" onClick={() => setUserIndex(i)}>
-            <h3>{user.name}</h3>
-          </button>
-        </li>
-      ))}
-    </ul>
+    <Fragment>
+      <ul className="users items-list-nav">
+        {users.map((user, i) => (
+          <li key={user.id} className={i === userIndex ? 'selected' : null}>
+            <button className="btn" onClick={() => setUserIndex(i)}>
+              {user.name}
+            </button>
+          </li>
+        ))}
+      </ul>
+      {user && (
+        <div className="bookable-details">
+          <div className="item">
+            <div className="item-header">
+              <h2>{user.name}</h2>
+            </div>
+            <div className="item-details">
+              <h3>{user.title}</h3>
+              <p>{user.notes}</p>
+            </div>
+          </div>
+        </div>
+      )}
+    </Fragment>
   )
 }
