68f8ed72062051d3219d0616d78996efa11ab6e1
Augusto Acual <augustoacual@gmail.com>
2023-04-27 21:42:34 -0600

    Related state

--- src/components/Bookables/BookablesList.jsx
+++ src/components/Bookables/BookablesList.jsx
@@ -12,6 +12,11 @@ export default function BookablesList() {
 
   const [hasDetails, setHasDetails] = useState(false)
 
+  function changeGroup(event) {
+    setGroup(event.target.value)
+    setBookableIndex(0)
+  }
+
   function nextBookable() {
     setBookableIndex((i) => (i + 1) % bookablesInGroup.length)
   }
@@ -19,7 +24,7 @@ export default function BookablesList() {
   return (
     <Fragment>
       <div>
-        <select value={group} onChange={(e) => setGroup(e.target.value)}>
+        <select value={group} onChange={changeGroup}>
           {groups.map((g) => (
             <option value={g} key={g}>
               {g}
