0be7d13a2b247d579e061cdbd9219b0f1a6a2d2b
Augusto Acual <augustoacual@gmail.com>
2023-05-09 15:42:11 -0600

    Passing an initialization function to the useReducer hook

--- src/components/Bookings/BookingsPage.jsx
+++ src/components/Bookings/BookingsPage.jsx
@@ -1,7 +1,10 @@
+import WeekPicker from './WeekPicker.jsx'
+
 export default function BookingsPage() {
   return (
     <main className="bookings-page">
       <p>Bookings!</p>
+      <WeekPicker date={new Date()} />
     </main>
   )
 }
